b01ecaa57903e187efb4a91d5a58298513508f79
mswinkels09 <mswinkels09@gmail.com>
2020-11-23 10:54:00 -0600

    finished 1-5

---
+++ 1-5/brazil_customers.sql
@@ -0,0 +1,4 @@
+--Provide a query only showing the Customers from Brazil.
+
+SELECT c.* FROM Customer c
+WHERE c.Country = "Brazil";
---
+++ 1-5/sales_agents.sql
@@ -0,0 +1,4 @@
+--Provide a query showing only the Employees who are Sales Agents.
+
+Select e.* From Employee e
+Where title = "Sales Support Agent";
